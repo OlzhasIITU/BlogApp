0f437653fc096c9392c96af3099ccd941583fc55
diasd <ahmediarlyo@gmail.com>
2026-03-03 13:28:34 +0500

    Changes Array1, fixed problems

--- src/For_While/Cycle3.java
+++ src/For_While/Cycle3.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class Cycle3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int n = sc.nextInt();
+        int counter = 1;
+        for (int i = 1; i < n+1 ; i++) {
+            counter = counter * i;
+        }
+        System.out.println(counter);
     }
 }
